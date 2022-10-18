c57d538a5b2bd8fb9b77165080108e1cdb7a020f
Oliver Walters <oliver.henry.walters@gmail.com>
2022-10-18 20:53:51 +1100

    Initial project commit
    - Setup script
    - Basic serial number management code

---
+++ setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+from sflserial.sfl_serial import SFL_PLUGIN_VERSION
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="inventree-brother-plugin",
+
+    version=SFL_PLUGIN_VERSION,
+
+    author="Oliver Walters",
+
+    author_email="oliver.henry.walters@gmail.com",
+
+    description="Custom serial number generation plugin for InvenTree",
+
+    long_description=long_description,
+
+    long_description_content_type='text/markdown',
+
+    keywords="inventree label printer printing inventory",
+
+    url="https://github.com/inventree/inventree",
+
+    license="MIT",
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+    ],
+
+    setup_requires=[
+        "wheel",
+        "twine",
+    ],
+
+    python_requires=">=3.7",
+
+    entry_points={
+        "inventree_plugins": [
+            "SFLSerialNumber = sflserial.sfl_serial:SFLSerialPlugin",
+        ]
+    },
+)
---
+++ sflserial/sfl_serial.py
@@ -0,0 +1,120 @@
+"""Custom serial number generation plugin for InvenTree
+
+Provides serial number generation and validation as specified by Toronto Space Flight Labs
+
+This software is distributed under the MIT license (see LICENSE file)
+"""
+
+SFL_PLUGIN_VERSION = '0.1.0'
+
+# Generic libraries
+import string
+
+# Django libraries
+from django.core.exceptions import ValidationError
+
+# InvenTree plugin libraries
+from plugin import InvenTreePlugin
+from plugin.mixins import SettingsMixin, ValidationMixin
+
+
+class SFLSerialNumberPlugin(SettingsMixin, ValidationMixin, InvenTreePlugin):
+    """Serial number generation and validation plugin.
+    
+    Serial numbers should be formatted like:
+
+    - AAA
+    - AAB
+    - AAC
+    - ...
+    - ABA
+    - ...
+    - ZZZ
+    - AAAA
+    
+    """
+
+    # Plugin metadata
+    NAME = "SFL Serial"
+    TITLE = "SFL Serial Number Generator"
+    DESCRIPTION = "Serial number generation plugin for Toronto Space Flight Labs"
+    SLUG = "sflserial"
+    VERSION = SFL_PLUGIN_VERSION
+
+    # No custom settings currently, but can be expanded as required
+    SETTINGS = {}
+
+    def validate_serial_number(self, serial: str):
+        """Serial number validation routine:
+        
+        - Must be at least three characters long
+        - Must only consist of characters A-Z
+        """
+        
+        if len(serial) < 3:
+            raise ValidationError("Serial number must be at least three characters")
+
+        for c in serial:
+            if c not in string.ascii_uppercase:
+                raise ValidationError("Serial number can only contain characters A-Z")
+        
+    def convert_serial_to_int(self, serial: str):
+        """Convert a serial number (string) to an integer representation.
+        
+        Essentially this serial number scheme is a "base 26" number?
+        
+        Iterate through each character, and if we find a "weird" character, simply return None
+        """
+
+        num = 0
+
+        # Reverse iterate through the serial number string
+        for idx, c in enumerate(serial[::-1]):
+            if c not in string.ascii_uppercase:
+                # An invalid character, not sure how to continue
+                # Also, should not ever get here due to validate_serial_number routine
+                return None
+
+            c_int = ord(c) - ord('A')
+            c_int *= (26 ** idx)
+
+            num += c_int
+        
+        return num
+
+    def increment_serial_number(self, serial: str):
+        """Find the next serial number in the required sequence
+
+        e.g.
+        AAA -> AAB
+        AAZ -> ABA
+        DQX -> DQY
+        ZZZ -> AAAA
+        """
+
+        if serial is None:
+            # Provide an initial condition
+            return "AAA"
+
+        output = ''
+        rollover = False
+
+        for c in serial[::-1]:
+            # If any character is invalid, return immediately
+            if c not in string.ascii_uppercase:
+                return None
+            
+            if not rollover:
+                if c == 'Z':
+                    c = 'A'
+                else:
+                    rollover = True
+                    c = chr(ord(c) + 1)
+            
+            output = c + output
+        
+        # If we get to the end of the sequence without incrementing, add a new character
+        if not rollover:
+            output = 'A' + output
+        
+        return output
